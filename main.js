a4e38a4b2624645946572d63099de977b7a17e23
linckn <linckn@umich.edu>
2016-08-06 22:09:18 -0400

    Added footer and header

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var main = function(){
     
-    };
+	$('.slider').click(function(){
+		var currentSlide = $('.currentSlide');
+	    var nextSlide = currentSlide.next();
 
+	    if(nextSlide.length === 0) {
+	      nextSlide = $('.slide').first();
+	    }
+	    
+	    currentSlide.fadeOut(600).removeClass('currentSlide');
+	    nextSlide.fadeIn(600).addClass('currentSlide');
+
+	});
+
+	
+
+};
 
 $(document).ready(main);
