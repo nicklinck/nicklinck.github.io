4c0ef41393917101cea0875f4777c80653bbf046
linckn <linckn@umich.edu>
2016-08-09 18:21:06 -0400

    created sticky footer

--- main.js
+++ main.js
@@ -26,6 +26,30 @@ var main = function(){
 
 	});	
 
+	$(document).keydown(function(event){
+		if( event.which === 39){
+			var currentSlide = $('.currentSlide');
+		    var nextSlide = currentSlide.next();
+
+		    if(nextSlide.length === 0) {
+		      nextSlide = $('.slide').first();
+		    }
+		    
+		    currentSlide.fadeOut(600).removeClass('currentSlide');
+		    nextSlide.fadeIn(600).addClass('currentSlide');
+		}
+		else if(event.which === 37){
+			var currentSlide = $('.currentSlide');
+		    var nextSlide = currentSlide.prev();
+
+		    if(nextSlide.length === 0) {
+		      nextSlide = $('.slide').last();
+		    }
+		    
+		    currentSlide.fadeOut(600).removeClass('currentSlide');
+		    nextSlide.fadeIn(600).addClass('currentSlide');
+		}
+	});	
 
 
 };
