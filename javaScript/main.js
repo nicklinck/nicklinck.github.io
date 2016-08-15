c13ffd7f7456b4fa2da90e472df8dffef2348a9f
linckn <linckn@umich.edu>
2016-08-15 18:05:34 -0400

    updated a lot more of the design

---
+++ javaScript/main.js
@@ -0,0 +1,58 @@
+var main = function(){
+    
+
+    // go through pictures on slide bar
+	$('.rightArrow').click(function(){
+		var currentSlide = $('.currentSlide');
+	    var nextSlide = currentSlide.next();
+
+	    if(nextSlide.length === 0) {
+	      nextSlide = $('.slide').first();
+	    }
+	    
+	    currentSlide.fadeOut(600).removeClass('currentSlide');
+	    nextSlide.fadeIn(600).addClass('currentSlide');
+
+	});
+
+	$('.leftArrow').click(function(){
+		var currentSlide = $('.currentSlide');
+	    var nextSlide = currentSlide.prev();
+
+	    if(nextSlide.length === 0) {
+	      nextSlide = $('.slide').last();
+	    }
+	    
+	    currentSlide.fadeOut(600).removeClass('currentSlide');
+	    nextSlide.fadeIn(600).addClass('currentSlide');
+
+	});	
+
+	$(document).keydown(function(event){
+		if( event.which === 39){
+			var currentSlide = $('.currentSlide');
+		    var nextSlide = currentSlide.next();
+
+		    if(nextSlide.length === 0) {
+		      nextSlide = $('.slide').first();
+		    }
+		    
+		    currentSlide.fadeOut(600).removeClass('currentSlide');
+		    nextSlide.fadeIn(600).addClass('currentSlide');
+		}
+		else if(event.which === 37){
+			var currentSlide = $('.currentSlide');
+		    var nextSlide = currentSlide.prev();
+
+		    if(nextSlide.length === 0) {
+		      nextSlide = $('.slide').last();
+		    }
+		    
+		    currentSlide.fadeOut(600).removeClass('currentSlide');
+		    nextSlide.fadeIn(600).addClass('currentSlide');
+		}
+	});	
+
+};
+
+$(document).ready(main);
